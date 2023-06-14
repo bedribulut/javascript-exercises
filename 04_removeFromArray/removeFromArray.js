9b874463756a4543d9771324b5ce56905a015e1d
Bedri Bulut <bedribulut93@gmail.com>
2023-06-14 18:48:44 +0300

    passed all the tests of removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    console.log(arr);
+    console.log(args);
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!args.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
