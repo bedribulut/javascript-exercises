d549dfa62e7378e807db12690336b83f47d5ea02
Bedri Bulut <bedribulut93@gmail.com>
2023-06-14 10:36:25 +0300

    reverseString passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const arr = str.split("");
+    const length = arr.length;
+    let newArr = [];
+
+    for (let i = length - 1; i >= 0; i--) {
+        newArr.push(arr[i]);
+    }
+    return newArr.join("");
 
 };
 
