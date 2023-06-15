357ce6336255adaa351d337704feaf046042c877
Bedri Bulut <bedribulut93@gmail.com>
2023-06-15 22:11:07 +0300

    sumAll passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    console.log(a);
+    console.log(b);
+    let total = 0;
+    if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    } else if (a < b) {
+        for (let i = a; i <= b; i++) {
+            console.log(i);
+            total = total + i;
+        }
+        return total;
+    } else if (b < a) {
+        for (let i = b; i <= a; i++) {
+            console.log(i);
+            total = total + i;
+        }
+        return total;
+    } else {
+        return "ERROR";
+    }
 
 };
 
