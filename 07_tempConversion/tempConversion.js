d418bdfe5c83f93682b9fae04ec10e21c763eefb
Bedri Bulut <bedribulut93@gmail.com>
2023-06-16 11:40:43 +0300

    tempConversion passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  const c = ((x - 32) * (5 / 9));
+  return Number.isInteger(c) ? c : +c.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+  const f = (y * (9 / 5) + 32);
+  return Number.isInteger(f) ? f : +f.toFixed(1);
 };
 
 // Do not edit below this line
